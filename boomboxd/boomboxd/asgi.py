b8cb3532e8b10d30cc55f90baadab5cea78b4779
Danalajmi <danaralajami@gmail.com>
2026-02-11 14:47:23 +0300

    change start cmd

--- boomboxd/boomboxd/asgi.py
+++ boomboxd/boomboxd/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'boomboxd.boomboxd.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'boomboxd.settings')
 
 application = get_asgi_application()
